c907b2a0d1c3f1580372569aba39724953a57bda
Andrenickolands <nicosepulvedaamaya@gmail.com>
2024-03-04 20:28:19 -0500

    Carga

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+    const music = document.getElementById("music");
+    const playPause = document.getElementById("play");
+
+    //playPause.addEventListener("click", () {
+    //    if(music.paused || music.ended) {
+    //        playPause.querySelector("play-btn").classList.toggle("hide");
+    //        playPause.querySelector("pause-btn").classList.toggle("hide");
+    //        music.play();
+    //    } else { 
+    //        music.pause();
+    //        playPause.querySelector("pause-btn").classList.toggle("hide");
+    //        playPause.querySelector("play-btn").classList.toggle("hide");
+     //   }
+   // });
