9a352a2cdf70d1c60b6c7ced31a3ae30af4557b8
Andrenickolands <nicosepulvedaamaya@gmail.com>
2024-11-04 17:30:07 -0500

    Nicolas

--- App/MediaPlayer/scripts.js
+++ App/MediaPlayer/scripts.js
@@ -1,23 +1,10 @@
-
-
-    const music = document.getElementById("music");
-
     const back = document.getElementById("back");
     const search = document.getElementById("search");
-    const menu = document.getElementById("menu");
+    const options = document.getElementById("options");
 
     const  backward = document.getElementById("backward");
+    const music = document.getElementById("music");
     const playPause = document.getElementById("play");
     const forward = document.getElementById("forward");
 
-    //playPause.addEventListener("click", () {
-    //    if(music.paused || music.ended) {
-    //        playPause.querySelector("play-btn").classList.toggle("hide");
-    //        playPause.querySelector("pause-btn").classList.toggle("hide");
-    //        music.play();
-    //    } else { 
-    //        music.pause();
-    //        playPause.querySelector("pause-btn").classList.toggle("hide");
-    //        playPause.querySelector("play-btn").classList.toggle("hide");
-     //   }
-   // });
+    
