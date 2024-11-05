d60cd9c9904d0f2ab23419e8d15c871720a9793a
Andrenickolands <nicosepulvedaamaya@gmail.com>
2024-11-04 20:36:36 -0500

    Nicolas

--- App/Explore/Explore.js
+++ App/Explore/Explore.js
@@ -7,6 +7,10 @@ const back = document.querySelector("#back");
 const search = document.querySelector("#search");
 const options = document.querySelector("#options");
 
+//DESPLEGABLE
+const desplegable = document.querySelector("#desplegable-explore");
+const deslizador = document.querySelector("#deslizador");
+
 back.addEventListener("click", () => {
     audio.play();
   })
@@ -18,4 +22,8 @@ back.addEventListener("click", () => {
   options.addEventListener("click", () => {
     audio.play();
     desplegable.classList.add("visible");
+  })
+
+  deslizador.addEventListener("click", () => {
+    desplegable.classList.remove("visible");
   })
--- App/List/List.js
+++ App/List/List.js
@@ -2,10 +2,17 @@
 const audioInput = new Audio('../../assets/audio/click__input.mp3');
 const audio = new Audio('../../assets/audio/click.mp3');
 
-//BOTONES
+//BOTONES options-album
 const back = document.querySelector("#back");
 const search = document.querySelector("#search");
 const options = document.querySelector("#options");
+const optionsAlbum = document.querySelector("#options-album");
+
+//DESPLEGABLE desplegable-album
+const desplegableAlbum = document.querySelector("#desplegable-album");
+const deslizadorAlbum = document.querySelector("#deslizador-album");
+const desplegable = document.querySelector("#desplegable");
+const deslizador = document.querySelector("#deslizador");
 
 back.addEventListener("click", () => {
     audio.play();
@@ -15,7 +22,19 @@ search.addEventListener("click", () => {
     audioInput.play();
 })
 
-options.addEventListener("click", () => {
+optionsAlbum.addEventListener("click", () => {
     audio.play();
-    desplegable.add("visible");
+    desplegableAlbum.classList.add("visible");
+})
+
+deslizadorAlbum.addEventListener("click", () => {
+    desplegableAlbum.classList.remove("visible");
+})
+
+options.addEventListener("click", () => {
+    desplegable.classList.add("visible");
+})
+
+deslizador.addEventListener("click", () => {
+    desplegable.classList.remove("visible");
 })
