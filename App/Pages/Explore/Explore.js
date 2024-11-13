e1479c2077ca281050870e9a04fe41566afc5fb1
Andrenickolands <nicosepulvedaamaya@gmail.com>
2024-11-13 10:26:01 -0500

    Nicolas correciones 1

--- App/Pages/Explore/Explore.js
+++ App/Pages/Explore/Explore.js
@@ -1,7 +1,51 @@
 
+//SONIDO
+const audioInput = new Audio('../../../assets/audio/click__input.mp3');
+const audio = new Audio('../../../assets/audio/click.mp3');
+
+//BOTONES
+const back = document.querySelector("#back");
+const search = document.querySelector("#search");
+
+//DESPLEGABLE
+const desplegable = document.querySelector("#desplegable-explore");
+const deslizador = document.querySelector("#deslizador");
+
+let isResizing = false;
+let startY;
+let startHeight;
+
+back.addEventListener("click", () => {
+  audio.play();
+  window.history.back();
+})
+
+search.addEventListener("click", () => {
+  audioInput.play();
+  window.location.href = "../Search/Search.html";
+})
+
+////////RESIZEZ////////
+
+deslizador.addEventListener('mousedown', (e) => {
+  isResizing = true;
+  startY = e.clientY;
+  startHeight = desplegable.clientHeight;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isResizing) return;
+  const dy = e.clientY - startY - startHeight;
+  desplegable.style.height = Math.max(startHeight + dy, 16) + '%';
+  desplegable.style.height = Math.min(startHeight + dy, 95) + '%';
+});
+
+document.addEventListener('mouseup', () => {
+  isResizing = false;
+});
 
 ////////WORLD////////
-// https://observablehq.com/@michael-keith/draggable-globe-in-d3@203
+https://observablehq.com/@michael-keith/draggable-globe-in-d3@203
 function _1(md){return(
   md``
   )}
@@ -82,7 +126,6 @@ function _1(md){return(
       .style("opacity",0.8)
   
     //Optional rotate
-
   
   }
   
